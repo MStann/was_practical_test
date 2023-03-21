b07c5bed0487544e3f4adf7ab8046fd4591b05ca
MStann <2838815s@student.gla.ac.uk>
2023-03-21 16:16:53 +0000

    Added order by date

--- was_practical_test/schoolnews/views.py
+++ was_practical_test/schoolnews/views.py
@@ -3,9 +3,8 @@ from schoolnews.models import Page
 
 # Create your views here.
 def index(request):
-    page_list = Page.objects.all()
+    page_list = Page.objects.order_by('-date')
 
-    context_dict = {}
-    context_dict['pages'] = page_list
+    context_dict = {'pages': page_list}
 
     return render(request, 'schoolnews/index.html', context=context_dict)
